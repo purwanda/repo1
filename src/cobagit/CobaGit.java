c9291bc5b84c98196ecaf25461a376bea53a19e1
wanda <purwanda_day@yahoo.com>
2017-10-28 06:24:06 +0700

    tambah file welcome

--- src/cobagit/CobaGit.java
+++ src/cobagit/CobaGit.java
@@ -16,6 +16,7 @@ public class CobaGit {
      */
     public static void main(String[] args) {
         show();
+        welcome.showwelcome();
         showselesai();
     }
     
---
+++ src/cobagit/welcome.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobagit;
+
+/**
+ *
+ * @author kanjeng
+ */
+public class welcome {
+    public static void showwelcome(){
+        System.out.println("selamat datang");
+    }
+}
